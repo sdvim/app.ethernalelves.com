b11272d7f16d2769b13cfb52da68b628ec2a7808
Steve Della Valentina <s.dellavalentina@gmail.com>
2022-01-25 17:39:06 -0700

    Refactor pages to .tsx; add new font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,16 @@ export default function Home() {
   return (
     <div className="Home page">
       <h1>Home</h1>
+      <button>Mint 1 Elf</button>
       <h2>Idle</h2>
       <div className="tmp-grid">
         { Array(8).fill(1).map((v, i) => <Avatar key={`i-${i}`} />) }
       </div>
-      <h2>On Campaign</h2>
+      <h2>Campaign</h2>
       <div className="tmp-grid">
         { Array(8).fill(1).map((v, i) => <Avatar key={`c-${i}`} />) }
       </div>
-      <h2>On Passive</h2>
+      <h2>Passive</h2>
       <div className="tmp-grid">
         { Array(8).fill(1).map((v, i) => <Avatar key={`p-${i}`} />) }
       </div>
