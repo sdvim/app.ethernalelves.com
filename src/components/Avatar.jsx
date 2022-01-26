a170a1bbfe86ad2a5aa180e0434334accab4094b
Steve Della Valentina <s.dellavalentina@gmail.com>
2022-01-25 19:08:43 -0700

    Add level to Avatar

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="Avatar">
       <img className="Avatar__image" src={elfImageData} alt="Avatar of Elf" />
+      <div className="Avatar__level">Lv.10</div>
       <div className="Avatar__bar Avatar__health"></div>
       <div className="Avatar__bar Avatar__experience"></div>
     </div>
