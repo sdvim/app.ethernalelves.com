7bca78e953187eeaf77340b8a0e082d1442b64d8
Steve Della Valentina <s.dellavalentina@gmail.com>
2022-01-26 17:05:18 -0700

    Add placeholder content for Help page

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -1,5 +1,17 @@
 export default function Help() {
   return (
-    <h1>Help</h1>
+    <div className="Help page">
+      <h1>Help</h1>
+      <h2>FAQ</h2>
+      
+      <h4>Pellentesque at mauris tellus.</h4>
+      <p>Curabitur aliquam auctor nulla vitae lobortis. Mauris consectetur velit est, quis consectetur dolor viverra et.</p>
+      
+      <h4>Ut porta fringilla elit, ut consectetur risus.</h4>
+      <p>Nam posuere justo nisl, quis lobortis justo rhoncus quis. Praesent ac justo vel lacus pulvinar congue.</p>
+
+      <h4>In ut mauris in mi pulvinar fringilla.</h4>
+      <p>Suspendisse nibh neque, sollicitudin nec est vel, iaculis placerat mauris.</p>
+    </div>
   );
 }
