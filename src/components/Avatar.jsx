3303723fd2b4737a0fc265a42fba503a22e82b86
Steve Della Valentina <s.dellavalentina@gmail.com>
2022-03-10 16:33:10 -0700

    Update theme

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -3,7 +3,7 @@ import "./Avatar.scss";
 export default function Avatar(props) {
   const isSelected = props.isSelected ? "Avatar--is-selected" : "";
   const degrees = 360 * (1 - (props.healthPercentage / 100));
-  const background = `conic-gradient(from 180deg, black ${degrees}deg, transparent 0)`;
+  const background = `conic-gradient(from 180deg, rgb(var(--cooldown-color)) ${degrees}deg, transparent 0)`;
   return (
     <div className={`Avatar ${isSelected}`} onClick={props.onClick}>
       <img className="Avatar__image" src={props.image} alt="Avatar of Elf" />
